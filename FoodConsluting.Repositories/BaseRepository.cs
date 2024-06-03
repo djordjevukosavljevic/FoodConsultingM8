7edb4653628390f490386a83efc9ccc863de36d4
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-06-03 14:14:50 +0200

    Repositories updates

---
+++ FoodConsluting.Repositories/BaseRepository.cs
@@ -0,0 +1,22 @@
+using System.Buffers.Text;
+
+namespace FoodConsluting.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : Base
+    {
+        TEntity GetOne(int id);
+    }
+
+    public class BaseRepo BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : Base
+    {
+        private readonly IConfiguration configuration;
+
+        public string coonectionString;
+
+        public MySqlConnectio connection;
+
+        public BaseRepository(IConfiguration configuration){
+            
+        }
+    }
+}
