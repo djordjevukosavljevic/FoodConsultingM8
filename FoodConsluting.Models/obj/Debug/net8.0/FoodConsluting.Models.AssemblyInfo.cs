b3d814bd124ac3dac729c2d18af0106a799499d2
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-06-07 17:34:32 +0200

    Adding background wallpaper.. not fully added

--- FoodConsluting.Models/obj/Debug/net8.0/FoodConsluting.Models.AssemblyInfo.cs
+++ FoodConsluting.Models/obj/Debug/net8.0/FoodConsluting.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34448ee63e8fe77f75caa7f91ab1037b1546daaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bfa304d503aab76dba70e51443966bfab645427")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
