f43f4a43511dd0793b21685dbede20f9c6543a03
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-07-26 21:37:58 +0200

    Removed view

--- FoodConsluting.Services/obj/Debug/net8.0/FoodConsluting.Services.AssemblyInfo.cs
+++ FoodConsluting.Services/obj/Debug/net8.0/FoodConsluting.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e609f3ed2e7c2512655e573b2c92c533ceb4be91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd4078831906c2ae2902009993db0a39286ac9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
