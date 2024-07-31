a2bbb337727488ef49fca03d5907bdb9bfda21f0
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-07-31 13:01:30 +0200

    Removed button in About view

--- FoodConsluting.Services/obj/Debug/net8.0/FoodConsluting.Services.AssemblyInfo.cs
+++ FoodConsluting.Services/obj/Debug/net8.0/FoodConsluting.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd4078831906c2ae2902009993db0a39286ac9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0c34f15a952bd8ea5c93d8c2218eb1535aadf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
