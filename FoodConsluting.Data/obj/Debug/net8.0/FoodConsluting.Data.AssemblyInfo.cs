6c5b85ba9e67ed243da018eaeff39d29e683dec8
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-08-07 13:43:48 +0200

    Small edits

--- FoodConsluting.Data/obj/Debug/net8.0/FoodConsluting.Data.AssemblyInfo.cs
+++ FoodConsluting.Data/obj/Debug/net8.0/FoodConsluting.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb3f0cbd06da3f3cec1a396ac45561b1a292197d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e673cda2e7c532ec7e14e45d2a0d837c739f4504")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
