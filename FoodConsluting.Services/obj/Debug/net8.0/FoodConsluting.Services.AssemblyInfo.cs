097aed7367643def76da806fb0bb2b9d205fad04
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-08-09 13:25:15 +0200

    Updates

--- FoodConsluting.Services/obj/Debug/net8.0/FoodConsluting.Services.AssemblyInfo.cs
+++ FoodConsluting.Services/obj/Debug/net8.0/FoodConsluting.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e673cda2e7c532ec7e14e45d2a0d837c739f4504")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5b85ba9e67ed243da018eaeff39d29e683dec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
