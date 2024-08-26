b9e75735154288dd59554b12809c911d70a6a15b
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-08-26 21:20:05 +0200

    User changes

--- FoodConsluting.Data/User.cs
+++ FoodConsluting.Data/User.cs
@@ -7,7 +7,9 @@ namespace FoodConsluting.Data{
         public string Name { get; set;}
         public string Email { get; set;}
 
-        public string Password{ get; set;}
+        public string Lastname { get; set; }
+
+        public string Password { get; set;}
 
 
         public enum USER_TYPE{
