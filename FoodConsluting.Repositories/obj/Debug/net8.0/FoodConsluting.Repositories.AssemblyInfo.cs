44fcf02eeef3fa2165f82060f9b3a7a27a4e7e29
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-08-30 18:34:44 +0200

    Still much bugs

--- FoodConsluting.Repositories/obj/Debug/net8.0/FoodConsluting.Repositories.AssemblyInfo.cs
+++ FoodConsluting.Repositories/obj/Debug/net8.0/FoodConsluting.Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96223f2fc1ec1a51f2b117947773a18a80b8dfd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eda9e303aa20d384232f684c5e634beb94a7176")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
