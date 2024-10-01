a596fa95c268c07b5c279a01aa098d7edb3fa5ba
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-02 01:00:39 +0200

    Minor changes

--- FoodConsDev/Controllers/HomeController.cs
+++ FoodConsDev/Controllers/HomeController.cs
@@ -7,34 +7,27 @@ namespace FoodConsDev.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
-        
         public IActionResult About(){
             return View();
         }
-
         public IActionResult Recommendations(){
             return View();
         }
-
         public IActionResult More(){
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
