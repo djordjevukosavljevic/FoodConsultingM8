709cf7e7dd5cd0ab1a32444eaafa8cecdad180dc
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-02 20:33:49 +0200

    Minor

--- FoodConsDev/Controllers/HomeController.cs
+++ FoodConsDev/Controllers/HomeController.cs
@@ -28,10 +28,6 @@ namespace FoodConsDev.Controllers
         public IActionResult More(){
             return View();
         }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
