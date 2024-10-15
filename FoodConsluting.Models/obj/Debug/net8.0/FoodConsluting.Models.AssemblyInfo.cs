d04e316766d4fedb19f93cca25878673f8b10972
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-15 19:29:50 +0200

    .net changes

--- FoodConsluting.Models/obj/Debug/net8.0/FoodConsluting.Models.AssemblyInfo.cs
+++ FoodConsluting.Models/obj/Debug/net8.0/FoodConsluting.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3001afc32d604b6d8b65b342a25b3db87d333f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b10b9dcd9097ec173fdf1a90deec42121e6479e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
