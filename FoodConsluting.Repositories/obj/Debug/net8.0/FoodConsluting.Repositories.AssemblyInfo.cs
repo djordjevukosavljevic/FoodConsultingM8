8b10b9dcd9097ec173fdf1a90deec42121e6479e
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-15 18:54:42 +0200

    dotnet 8.0 changes

--- FoodConsluting.Repositories/obj/Debug/net8.0/FoodConsluting.Repositories.AssemblyInfo.cs
+++ FoodConsluting.Repositories/obj/Debug/net8.0/FoodConsluting.Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709cf7e7dd5cd0ab1a32444eaafa8cecdad180dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3001afc32d604b6d8b65b342a25b3db87d333f7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
