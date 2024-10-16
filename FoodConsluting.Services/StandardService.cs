4ef850e9c5ead49ae7503c478fed905e079cd2dc
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-16 20:13:42 +0200

    Service changes

--- FoodConsluting.Services/StandardService.cs
+++ FoodConsluting.Services/StandardService.cs
@@ -7,13 +7,16 @@ namespace FoodConsluting.Services
 {
     public interface IStandardService  
     {
-        
+
     
         public List<Standard> GetAll();
     }
 
     public class StandardService : IStandardService
     {
+        // private readonly IStandardRepository _repository;
+
+
         public List<Standard> GetAll()
         {
             throw new Exception("Bug");
