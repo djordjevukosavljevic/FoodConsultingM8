2922b129d2d769881a09b62cb258e0a8f758df9d
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-19 13:56:30 +0200

    Review added

--- FoodConsluting.Data/Review.cs
+++ FoodConsluting.Data/Review.cs
@@ -1,9 +1,21 @@
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FoodConsluting.Data
 {
-    public class Review{
-        public int Id { get; set;}
-        public User user{ get; set;}
-        public String Text{ get; set;}
-    }    
+
+    [Table("review")]
+    public class Review
+    {
+        [Column("review_id")]
+        public int Id { get; set; }
+        [Column("company_name")]
+        public string CompanyName { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        [Column("text")]
+        public string Text { get; set; }
+        public DateTime createdAt { get; set; }
+
+    }
 }
