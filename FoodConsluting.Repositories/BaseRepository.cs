bf7555f7a64e8ac12c7d225fe06e6e9206bdf03b
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-19 19:50:28 +0200

    Added crud base repsitory

--- FoodConsluting.Repositories/BaseRepository.cs
+++ FoodConsluting.Repositories/BaseRepository.cs
@@ -2,25 +2,27 @@
 using System.Buffers.Text;
 using AutoMapper;
 using Dapper;
+using FoodConsluting.Data;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 
 
 namespace FoodConsluting.Repositories
 {
-    public interface IBaseRepository<TEntity> 
+    public interface IBaseRepository<TEntity>
     {
-        List<TEntity> GetAll();     
-        TEntity GetOne();   
+        List<TEntity> GetAll();
+        TEntity GetOne(int id);
     }
 
-    public class BaseRepository<TEntity>  : IBaseRepository<TEntity> where TEntity : Base
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : Base
     {
         private readonly IConfiguration configuration;
         public string connectionString;
         public MySqlConnection connection;
 
-        public BaseRepository(IConfiguration configuration){
+        public BaseRepository(IConfiguration configuration)
+        {
             SimpleCRUD.SetDialect(SimpleCRUD.Dialect.MySQL);
             this.configuration = configuration;
             connectionString = configuration.GetConnectionString("DefaultConnection");
@@ -30,12 +32,17 @@ namespace FoodConsluting.Repositories
 
         public List<TEntity> GetAll()
         {
-            throw new NotImplementedException();
+            var result = connection.GetList<TEntity>().ToList();
+            return result;
+
         }
 
-        public TEntity GetOne()
+        public TEntity GetOne(int id)
         {
-            throw new NotImplementedException();
+            var result = connection.Get<TEntity>(id);
+            return result;
         }
+
+
     }
 }
