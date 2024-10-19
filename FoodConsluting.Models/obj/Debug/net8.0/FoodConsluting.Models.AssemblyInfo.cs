3dd44b1d6a3e375db8e728649bfb9737a7918c0f
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-19 18:52:25 +0200

    Gallery page added

--- FoodConsluting.Models/obj/Debug/net8.0/FoodConsluting.Models.AssemblyInfo.cs
+++ FoodConsluting.Models/obj/Debug/net8.0/FoodConsluting.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4770466d7d91edb87e872e48125b80fa32ed9a81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99beaad16c0d45c2fbd72cd43179db8cf0559b20")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
