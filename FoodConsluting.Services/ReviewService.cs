7da2b8a8fd32c25fd73ffe4565b68c95741d1155
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-20 08:30:53 +0200

    Review Repo changes

--- FoodConsluting.Services/ReviewService.cs
+++ FoodConsluting.Services/ReviewService.cs
@@ -1,20 +1,23 @@
+using FoodConsluting.Data;
 using AutoMapper;
+using FoodConsluting.Repositories;
+using ReviewRepository;
 
 namespace FoodConsluting.Services
 {
-    public interface IReviewService
+    public interface IReviewService : IBaseRepository<Review>
     {
 
     }
 
     public class ReviewService
     {
-        //private readonly IReviewRepository repository;
+        private readonly IReviewRepository repository;
         
         private IMapper mapper;
-        public ReviewService(/*IReviewRepository repository*/ IMapper mapper)
+        public ReviewService(IReviewRepository repository, IMapper mapper)
         {
-            //this.repository = repository;
+            this.repository = repository;
             this.mapper = mapper;
         }
     }
