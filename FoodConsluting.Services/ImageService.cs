16e9b6dfd30978851e126f31b096f30af9a42acc
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-10-28 23:56:09 +0100

    FoodConsultingServiceUpdate

--- FoodConsluting.Services/ImageService.cs
+++ FoodConsluting.Services/ImageService.cs
@@ -1,18 +1,40 @@
+using AutoMapper;
 using FoodConsluting.Data;
-using Microsoft.Extensions.Configuration;
+using FoodConsulting.Repositories;
 
-namespace FoodConsluting.Repositories
+namespace FoodConsluting.Services
 {
-    public interface IImageRepository : IBaseRepository<Image>
+    public interface IImageService
     {
+        List<Image> GetImages();
 
+        Image GetImageById(int id);
     }
 
-    public class ImageRepository : BaseRepository<Image>, IImageRepository
+    public class ImageService : IImageService
     {
-        public ImageRepository(IConfiguration configuration) : base(configuration)
+        private readonly IImageRepository repository;
+
+        private readonly IMapper mapper;
+
+        public ImageService(IImageRepository repository, IMapper mapper)
         {
-            
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public List<Image> GetImages()
+        {
+            var resultFromDb = repository.GetAll();
+            var result = mapper.Map<List<Image>>(resultFromDb);
+            return result;
+        }
+
+        public Image GetImageById(int id)
+        {
+            var resultFromDb = repository.GetOne(id);
+            var result = mapper.Map<Image>(resultFromDb);
+            return result;
         }
 
     }
